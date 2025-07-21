28da514d451bfbc6385289b52ab2f27699d124d1
Masamello <kojimasa1205@gmail.com>
2025-07-20 17:46:05 -0700

    1

---
+++ JavaScript/JavaScript/leetcode/p2620.js
@@ -0,0 +1,13 @@
+
+const createCounter = function(n){
+    let current = n;
+    return function(){
+        return current++
+    }
+}
+
+const counter = createCounter(10);
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
---
+++ JavaScript/JavaScript/leetcode/p2648.js
@@ -0,0 +1,19 @@
+const fiboGenerator = function*(){
+    yield 0;
+    yield 1;
+    while()
+}
+const gen = fiboGenerator();
+
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+
+// let result = [];
+//     let num = n;
+//     let sum = 0;
+//     for(let i = 0; i < num; i++){
+        
+//     }
