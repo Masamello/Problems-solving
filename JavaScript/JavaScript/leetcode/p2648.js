eca5fa417229b0e0eb68cd879f9c1abccd7b7179
Masamello <kojimasa1205@gmail.com>
2025-07-20 21:55:39 -0700

    p2648 solved

--- JavaScript/JavaScript/leetcode/p2648.js
+++ JavaScript/JavaScript/leetcode/p2648.js
@@ -1,18 +1,16 @@
 const fiboGenerator = function*(){
-    yield 0;
-    yield 1;
+    let a = 0, b = 1;
+    while (true) {
+        yield a;
+        [a, b] = [b, a + b];
+    }
+
 }
+
 const gen = fiboGenerator();
+for(let i = 0; i < 5; i++){
+    console.log(gen.next().value);
+}
 
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-
-// let result = [];
-//     let num = n;
-//     let sum = 0;
-//     for(let i = 0; i < num; i++){
-        
-//     }
+// function* => generator function,普通の関数と少し違う、yieldを使って処理一時的に停止、valueを返すことができる。
+// 初期値に0,1を設定、無限ループでaとbを更新しながら値を返す。返す値は最初のaのみなので、yieldはaだけを選択。
