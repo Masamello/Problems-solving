b32101b778418b68b4e36329d78b08f88d67be95
Masamello <kojimasa1205@gmail.com>
2025-08-06 14:29:43 -0700

    Q5,6,7,8 solved

--- JavaScript/JavaScript/problemByChatGPT/script.js
+++ JavaScript/JavaScript/problemByChatGPT/script.js
@@ -59,3 +59,87 @@ console.log('Count Island Ones:',countIslandOnes('110011100111'));
 console.log('Count Island Ones:',countIslandOnes('101010101'));
 console.log('Count Island Ones:',countIslandOnes('0000'));
 console.log('Count Island Ones:',countIslandOnes('11111'));
+
+// Write a function countBalancedSubstrings that takes stings which become only 1 and 0 and returns balanced substrings
+const countBalancedSubstrings = (str) =>{
+    let counter = 0;
+    let zero = 0;
+    let one = 0;
+    str.split("").forEach(char => {
+        if(char == '0'){
+            zero++; 
+        }else if(char == '1'){
+            one++;
+        }
+        if(zero == one){
+            counter++;
+            zero = 0;
+            one = 0;
+        }
+    });
+    return counter;
+}
+
+console.log('Balanced Binary String:',countBalancedSubstrings('01001110'));
+console.log('Balanced Binary String:',countBalancedSubstrings('000000'));
+console.log('Balanced Binary String:',countBalancedSubstrings('01'));
+console.log('Balanced Binary String:',countBalancedSubstrings('110100011'));
+
+// Write a function that takes str which become only 1 and 0 and returns longest alternating sequensce substrings without using array methods.
+const longestAlternatingSequence = (str)=>{
+    let currentLen = 1;
+    let maxLen = 1;
+    for(let i = 1; i < str.length; i++){
+        if(str[i] !== str[i - 1] && (str[i] === '0' || str[i] === '1')){
+            currentLen++;
+            if(currentLen > maxLen){
+                maxLen = currentLen;
+            }
+        }else{
+            currentLen = 1;
+        }
+    }
+    return maxLen;
+}
+
+console.log('Longest Alternating Seqeuce:',longestAlternatingSequence('101010'));
+console.log('Longest Alternating Seqeuce:',longestAlternatingSequence('110011'));
+console.log('Longest Alternating Seqeuce:',longestAlternatingSequence('000111'));
+console.log('Longest Alternating Seqeuce:',longestAlternatingSequence('101'));
+
+const longestParallelSequence = (str) =>{
+    let currentLen = 1;
+    let maxLen = 1;
+    for(let i = 1; i < str.length; i++){
+        if(str[i] === str[i - 1] && (str[i] === '1' || str[i] === '0')){
+            currentLen++;
+            if(currentLen > maxLen){
+                maxLen = currentLen;
+            }
+        }else{
+            currentLen = 1;
+        }
+    }
+    return maxLen;
+}
+
+console.log('Longest Parallel Sequence:',longestParallelSequence('11000111101010111'));
+console.log('Longest Parallel Sequence:',longestParallelSequence('000011000'));
+console.log('Longest Parallel Sequence:',longestParallelSequence('10101'));
+console.log('Longest Parallel Sequence:',longestParallelSequence('11111'));
+
+// Write a function isSymmetric that takes array and check it if that array are palindromic or not, return only true or false.
+const isSymmetric = (arr) =>{
+    let start = 0;
+    let last = arr.length - 1;
+    while(start < last){
+        if(arr[start] !== arr[last]) return false;
+        start++;
+        last--;
+    }
+    return true;
+}
+
+console.log('isSymmetric:',isSymmetric([1,2,3,2,1]));
+console.log('isSymmetric:',isSymmetric(["a","b","c","b","a"]));
+console.log('isSymmetric:',isSymmetric([1,2,3,4,5]));
